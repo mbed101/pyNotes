bd147e849a451d4e877cac61c1c31f2c36da3ae4
mbed101 <mahend101@protonmail.com>
2023-07-09 20:35:40 +0200

    added system

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,3 +1,5 @@
+#! /usr/bin/env python3
+
 def bubble_sort(arr):
     n = len(arr)
     
---
+++ System/ExecuteCommand.py
@@ -0,0 +1,17 @@
+import subprocess
+
+# Execute a shell command and capture its output
+def execute_command(command):
+    try:
+        output = subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT, universal_newlines=True)
+        return output
+    except subprocess.CalledProcessError as e:
+        # Handle command execution errors
+        print(f"Command execution failed with return code {e.returncode}: {e.output}")
+        return None
+
+# Example usage
+command = "ls -l"
+output = execute_command(command)
+if output:
+    print(f"Command output:\n{output}")
